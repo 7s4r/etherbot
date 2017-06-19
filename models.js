ed4ba71edb8361bd1988937c30174f2d7e15be9f
Romain Lofaso <romain.lofaso@gmail.com>
2017-06-19 22:55:42 +0200

    Fetch cryptocompare API to fulfill the database with ETHEU

--- models.js
+++ models.js
@@ -1,20 +1,8 @@
 'use strict'
 
-function Price(timestamp, bit, ask) {
+function Price(timestamp, value) {
   this.timestamp = timestamp
-  this.bit = bit
-  this.ask = ask
+  this.value = value
 }
 
-function Serializer() {
-}
-
-Serializer.prototype.deserialisePrice = function(raw) {
-  const isValid = Array.isArray(raw) && (raw.length == 3)
-  if (!isValid) {
-    throw new Error("Price raw data is crappy: " + raw)
-  }
-  return new Price(raw[0], raw[1], raw[2])
-}
-
-module.exports = { Price, Serializer }
+module.exports = { Price }
--- remote_api.js
+++ remote_api.js
@@ -1,24 +1,51 @@
 'use strict'
 
-const KrakenClient = require('kraken-api')
-const Promise = require('promise')
+const Sleep = require('sleep')
+const request = require('request-promise-native')
+const Price = require('./models.js').Price
 
 function RemoteAPI() {
-  this.client = new KrakenClient("blabla", "blabla");
+  this.beginingTimestamp = (new Date("2016-09-01").valueOf() / 1000)
+  this.endingTimestamp = (new Date("2017-06-19").valueOf() / 1000)
 }
 
-RemoteAPI.prototype.getFirstPagePrice = function() {
-  const self = this
-	const promise = new Promise(function (resolve, reject) {
-    self.client.api('Spread', {"pair": 'ETHEUR'}, function(error, data) {
-        if(error) {
-            reject(err)
-        } else {
-          resolve(data.result)
-        }
-    })
-	})
-	return promise
+RemoteAPI.prototype.fetchPriceHistory = async function(insertionManyPrices) {
+  var timestamp = this.endingTimestamp
+  while (timestamp >= this.beginingTimestamp) {
+    const url = "https://min-api.cryptocompare.com/data/histohour?fsym=ETH&tsym=EUR&toTs=" + timestamp
+    const str = await request(url)
+    const tuple = this.deserializeRequest(str)
+    await insertionManyPrices(tuple.prices)
+    timestamp = tuple.firstTimestamp // decrementation
+    this.updateProgress(timestamp, tuple.prices)
+    Sleep.sleep(2) // To avoid DoS
+  }
+}
+
+RemoteAPI.prototype.updateProgress = function(timestamp, prices) {
+  const relativeTimestamp = timestamp - this.beginingTimestamp
+  const relativeEndingTimestamp = this.endingTimestamp - this.beginingTimestamp
+  // The timestamp is decremented from endingTimestamp to beginingTimestamp
+  // So the numerator of the progression is inversed.
+  const progress = (relativeEndingTimestamp - relativeTimestamp) / relativeEndingTimestamp
+  const percent = progress * 100
+  const percentInt = Math.trunc(percent)
+  const date = new Date(timestamp * 1000)
+  const dateString = date.toISOString().substring(0, 10)
+  console.log("Progress: " + percentInt + "%\tInsert price from " + dateString + "\t" + prices.length + " items")
+}
+
+RemoteAPI.prototype.deserializeRequest = function(str) {
+  const json = JSON.parse(str)
+  const firstTimestamp = json["TimeFrom"]
+  const rawPrices = json["Data"]
+  const prices = rawPrices.map(function(rawPrice) {
+    const timestamp = rawPrice["time"]
+    const value = rawPrice["open"]
+    const price = new Price(timestamp, value)
+    return price
+  })
+  return { prices: prices, firstTimestamp: firstTimestamp }
 }
 
 module.exports = RemoteAPI
