d58c95c2d8f48560904e1188e6fab0795a209f04
Gueorgui Agapov <g.agapov@gmail.com>
2017-06-28 23:25:04 +0200

    fix redis

--- back/app.py
+++ back/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host='db', port=6379)
 
 @app.route('/')
 def hello():
